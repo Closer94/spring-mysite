da9dbe6fb70d0115ff817c957e2ad8ee4b08f40f
kabsu <kabsu@LAPTOP-VQRP8LS3>
2021-04-21 10:01:16 +0900

    2021_04_21 contextLoadListener

---
+++ mysite02/src/main/java/com/bitcademy/mysite/web/ContextLoadListener.java
@@ -0,0 +1,24 @@
+package com.bitcademy.mysite.web;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+
+public class ContextLoadListener implements ServletContextListener {
+	
+
+    // 톰캣에서 Application이 로딩될때
+    public void contextInitialized(ServletContextEvent servletContextEvent)  { 
+    	ServletContext context = servletContextEvent.getServletContext();
+    	String contextConfigLocation = context.getInitParameter("contextConfigLocation");
+    	System.out.println("Application start..." + contextConfigLocation);
+    }
+	
+    // 톰캣에서 Application이 내려갈때
+    public void contextDestroyed(ServletContextEvent sce)  { 
+
+    }
+
+	
+}
